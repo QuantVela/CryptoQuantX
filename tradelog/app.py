71050cd416910928e8d367cc9ef17593d7362f4f
linqianqian <autumn1006@foxmail.com>
2024-02-17 22:13:52 +0800

    0217 tradlog go on
    今天很长时间都在配置 cloudflare tunnels，其实不难就是要注意的点比较多，然后调了会 flask 代码，已经能写入表格了，剩下的明天继续。

--- tradelog/app.py
+++ tradelog/app.py
@@ -16,60 +16,63 @@ def read_sheet():
 
 @app.route('/add_tradelog', methods=['POST'])
 def add_tradelog():
-    if not request.is_json:
-        abort(400, 'Request must be JSON')
     
     data = request.get_json()
 
-    if 'entry_fill' in data:
-        for trade in data['entry_fill']:
-            trade_id = trade.get('trade_id', 'N/A')
-            pair = trade.get('pair', 'N/A')
-            direction = trade.get('direction', 'N/A')
-            leverage = trade.get('leverage', 'N/A')
-            open_rate = trade.get('open_rate', 'N/A')
-            amount = trade.get('amount', 'N/A')
-            open_date = trade.get('open_date', 'N/A')
-            stake_amount = trade.get('stake_amount', 'N/A')
-            stake_currency = trade.get('stake_currency', 'N/A')
-            base_currency = trade.get('base_currency', 'N/A')
-            fiat_currency = trade.get('fiat_currency', 'N/A')
-            order_type = trade.get('order_type', 'N/A')
-            current_rate = trade.get('current_rate', 'N/A')
-            enter_tag = trade.get('enter_tag', 'N/A')
+    if 'enter_tag' in data:
+        trade_id = data.get('trade_id', 'N/A')
+        pair = data.get('pair', 'N/A')
+        direction = data.get('direction', 'N/A')
+        leverage = data.get('leverage', 'N/A')
+        open_rate = data.get('open_rate', 'N/A')
+        amount = data.get('amount', 'N/A')
+        open_date = data.get('open_date', 'N/A')
+        stake_amount = data.get('stake_amount', 'N/A')
+        stake_currency = data.get('stake_currency', 'N/A')
+        base_currency = data.get('base_currency', 'N/A')
+        fiat_currency = data.get('fiat_currency', 'N/A')
+        order_type = data.get('order_type', 'N/A')
+        current_rate = data.get('current_rate', 'N/A')
+        enter_tag = data.get('enter_tag', 'N/A')
 
-            app.logger.debug(f"Logging trade: Trade ID: {trade_id}, Pair: {pair}, Direction: {direction}, 
-                             Leverage: {leverage}, Open Rate: {open_rate}, Amount: {amount}, 
-                             Open Date: {open_date}, Stake Amount: {stake_amount}, Stake Currency: {stake_currency}, 
-                             Base Currency: {base_currency}, Fiat Currency: {fiat_currency}, Order Type: {order_type}, 
-                             Current Rate: {current_rate}, Enter Tag: {enter_tag}")
+        app.logger.debug(f"""Logging trade: Trade ID: {trade_id}, Pair: {pair}, Direction: {direction}, 
+                            Leverage: {leverage}, Open Rate: {open_rate}, Amount: {amount}, 
+                            Open Date: {open_date}, Stake Amount: {stake_amount}, Stake Currency: {stake_currency}, 
+                            Base Currency: {base_currency}, Fiat Currency: {fiat_currency}, Order Type: {order_type}, 
+                            Current Rate: {current_rate}, Enter Tag: {enter_tag}""")
 
-            gsheet.append_row([trade_id, pair, open_date])
+        gsheet.append_row([trade_id, pair, open_date])
         return jsonify({"message": "Trade logged successfully"})
     
-    if 'exit_fill' in data:
-        for trade in data['exit_fill']:
-            trade_id = trade.get('trade_id', 'N/A')
-            pair = trade.get('pair', 'N/A')
-            direction = trade.get('direction', 'N/A')
-            leverage = trade.get('leverage', 'N/A')
-            gain = trade.get('gain', 'N/A')
-            close_rate = trade.get('close_rate', 'N/A')           
-            amount = trade.get('amount', 'N/A')
-            open_rate = trade.get('open_rate', 'N/A')
-            current_rate = trade.get('current_rate', 'N/A')
-            profit_amount = trade.get('profit_amount', 'N/A')
-            profit_ratio = trade.get('profit_ratio', 'N/A')
-            stake_currency = trade.get('stake_currency', 'N/A')
-            base_currency = trade.get('base_currency', 'N/A')
-            fiat_currency = trade.get('fiat_currency', 'N/A')
-            exit_reason = trade.get('exit_reason', 'N/A')
-            order_type = trade.get('order_type', 'N/A')
-            open_date = trade.get('open_date', 'N/A')
-            close_date = trade.get('close_date', 'N/A')
+    if 'exit_reason' in data:
+        trade_id = data.get('trade_id', 'N/A')
+        pair = data.get('pair', 'N/A')
+        direction = data.get('direction', 'N/A')
+        leverage = data.get('leverage', 'N/A')
+        gain = data.get('gain', 'N/A')
+        close_rate = data.get('close_rate', 'N/A')           
+        amount = data.get('amount', 'N/A')
+        open_rate = data.get('open_rate', 'N/A')
+        current_rate = data.get('current_rate', 'N/A')
+        profit_amount = data.get('profit_amount', 'N/A')
+        profit_ratio = data.get('profit_ratio', 'N/A')
+        stake_currency = data.get('stake_currency', 'N/A')
+        base_currency = data.get('base_currency', 'N/A')
+        fiat_currency = data.get('fiat_currency', 'N/A')
+        exit_reason = data.get('exit_reason', 'N/A')
+        order_type = data.get('order_type', 'N/A')
+        open_date = data.get('open_date', 'N/A')
+        close_date = data.get('close_date', 'N/A')
 
-            gsheet.append_row([trade_id, pair, open_date])
+        app.logger.debug(f"""Logging trade: Trade ID: {trade_id}, Pair: {pair}, Direction: {direction}, 
+                            Leverage: {leverage}, gain: {gain}, close_rate: {close_rate}, Amount: {amount}, 
+                            open_rate: {open_rate},  Current Rate: {current_rate}, profit_amount: {profit_amount},
+                            profit_ratio: {profit_ratio}, Stake Currency: {stake_currency}, Base Currency: {base_currency}, Fiat Currency: {fiat_currency},
+                            exit_reason: {exit_reason}, Order Type: {order_type}, Open Date: {open_date},       
+                            close_date: {close_date}""")
+        gsheet.append_row([trade_id, pair, open_date])
+
         return jsonify({"message": "Trade logged successfully"})
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', debug=True, port=5000)
+    app.run(host='0.0.0.0', debug=False, port=5000)
